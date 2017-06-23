ba6daa98db00baaa75c9c9d6c758cc3f691896ec
Halil Coban <halil.coban@gmail.com>
2017-06-23 00:05:08 -0700

    disabled debug mode

--- predictor/settings.py
+++ predictor/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG', default=False, cast=bool]
+DEBUG = False
 
 ALLOWED_HOSTS = ['cat-or-dog.herokuapp.com']
 
