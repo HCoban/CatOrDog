fbd450cf75f79a2380fb98be72c2ae073a1359c9
Halil Coban <halil@joinhandshake.com>
2017-07-18 09:12:19 -0700

    moved the form to index

--- cat_dog/views.py
+++ cat_dog/views.py
@@ -4,13 +4,10 @@ from .models import CatDog
 from django import forms
 
 def index(request):
-    animals = CatDog.objects.order_by('-id')[:5]
-    return render(request, "index.html", { 'animals': animals })
-
-def new(request):
     class ImageForm(forms.Form):
         image_location = forms.CharField(widget=forms.TextInput(attrs={'id':'image-location', 'class': 'hidden'}))
-    return render(request, "new.html", { "form": ImageForm() })
+    animals = CatDog.objects.order_by('-id')[:5]
+    return render(request, "index.html", { 'animals': animals, 'form': ImageForm() })
 
 def create(request):
     cat_dog = CatDog(path=request.POST['image_location'])
